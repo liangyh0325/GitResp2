a492044723a929faa613e44826b3de2ffe0d24ef
liangyh0325 <151374484+liangyh0325@users.noreply.github.com>
2024-08-12 22:34:26 +0800

    idea中第一次进行数据提交

---
+++ src/main/java/com/example/testdemo/Test.java
@@ -0,0 +1,13 @@
+package com.example.testdemo;
+
+/**
+ * ClassName: test
+ * Package: com.example.testdemo
+ * Description:
+ *
+ * @Author 梁宇豪
+ * @Create 2024-08-12 22:32
+ * @Version 1.0
+ */
+public class Test {
+}
